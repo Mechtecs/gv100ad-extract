c10fdb57aa3c9e8742b3c9ce93dd6e30ae7e78c0
Max Schorradt <schorradt@tech-effect.de>
2021-11-04 10:59:16 +0100

    Beautify converter php

--- extract-gv100ad.php
+++ extract-gv100ad.php
@@ -7,21 +7,9 @@
 
 $f = fopen('GV100AD_301121.ASC', 'r');
 
-
-//    120643405410
-//    12 Brandenburg
-//    0 -
-//    64 Landkreis Märkisch Oderland
-//    340 Gemeinde Neuhardenberg
-//    5410 Amt Neuhardenberg
-
 $list = [];
+$matches = null;
 while ($line = fgets($f)) {
-  $ags = substr($line, 10, 8);
-  $verband = substr($line, 18, 4);
-  $ars = substr($ags, 0, 5) . $verband . substr($ags, 5);
-
-  $matches = null;
   $formatCorrect = preg_match("/.{10}(\d{5})(\d{3})(\d{4})(.{50})(.{50}).{98}/", $line, $matches);
   if ($formatCorrect !== 1) {
       continue;
@@ -30,11 +18,11 @@ while ($line = fgets($f)) {
 
 
   $list[] = [
-    'ars' => trim($ars),
-    'name' => trim(substr($line, 22, 50)),
-    'gemeindeschluessel' => trim($ags),
-    'verbandsschluessel' => trim($verband),
-    'landesregierung' => trim(substr($line, 72, 50)),
+    'ars' => $matches[1] . $matches[3] . $matches[2],
+    'name' => trim($matches[4]),
+    'gemeindeschluessel' => $matches[1] . $matches[2],
+    'verbandsschluessel' => $matches[3],
+    'landesregierung' => trim($matches[5]),
   ];
 }
 
@@ -46,10 +34,10 @@ file_put_contents('ars.min.json', json_encode($list, JSON_PARTIAL_OUTPUT_ON_ERRO
 
 
 // SQL out
-$inserts = [];
-foreach ($list as $item) {
-  $inserts[] = "('{$item['ars']}', '{$item['gemeindeschluessel']}', '{$item['verbandsschluessel']}', '{$item['landesregierung']}', '{$item['name']}')";
-}
+$inserts = array_map(static function (array $item) {
+    return "('{$item['ars']}', '{$item['gemeindeschluessel']}', '{$item['verbandsschluessel']}', '{$item['landesregierung']}', '{$item['name']}')";
+}, $list);
+
 $out = "CREATE TABLE IF NOT EXISTS `ars` (`ars` VARCHAR(12) NOT NULL, `ags` VARCHAR(8) NOT NULL, `verband` VARCHAR(4) NULL DEFAULT NULL, `land` VARCHAR(50) NULL DEFAULT NULL, `name` VARCHAR(50) NULL DEFAULT NULL, PRIMARY KEY(`ars`));\n";
 $out .= "INSERT INTO `ars` (`ars`, `ags`, `verband`, `land`, `name`) VALUES \n" . implode(",\n", $inserts) . ";\n";
 
